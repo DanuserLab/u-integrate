74ecaf1f18974defd1e550b85459ea157e5e7d23
Danuser CI User <danuser_ci@NucleusB107.cm.cluster>
2024-01-25 21:20:38 -0600

    Danuser Lab CI pipeline build # 181467

--- software/ProtrusionSamplingProcess.m
+++ software/ProtrusionSamplingProcess.m
@@ -170,7 +170,7 @@ classdef ProtrusionSamplingProcess < ImageAnalysisProcess
             name = 'Protrusion Sampling';
         end
         function name =GUI()
-            name =@noSettingsProcessGUI;
+            name =@noSettingsProcessGUI_App;
         end
 
         function funParams = getDefaultParams(owner,varargin)
